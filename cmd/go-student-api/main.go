19385c0a4efbc14677f5c5b924e7978d45302630
Farooq Zahid <farooqinfocus@gmail.com>
2024-12-27 16:39:22 +0500

    Setup and ran Server

--- cmd/go-student-api/main.go
+++ cmd/go-student-api/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main(){
-	
+import (
+	"log"
+	"net/http"
+
+	"github.com/MuhammadFarooqZahid/go-student-api/internal/config"
+)
+
+func main() {
+	// Load config
+	cfg := config.MustLoad()
+
+	// Database setup
+	// Setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to student's api"))
+	})
+	// Setup server
+
+	log.Printf("Server is running at %s\n", cfg.Address)
+
+	err := http.ListenAndServe(cfg.Address, router)
+
+	if err != nil {
+		log.Fatalf("Failed to start server: %s", err.Error())
+	}
+
 }
---
+++ internal/config/config.go
@@ -0,0 +1,51 @@
+package config
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env         string `yaml:"env" env:"ENV" env-required:"true"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HttpServer  `yaml:"http_server"`
+}
+
+type HttpServer struct {
+	Address string
+}
+
+func MustLoad() *Config {
+
+	var configPath string
+
+	configPath = os.Getenv("CONFIG_PATH")
+
+	if configPath == "" {
+		flags := flag.String("config","","Path to configuration file")
+		flag.Parse()
+
+		configPath = *flags
+
+		if configPath == "" {
+			log.Fatal("Config path is not set")
+		}
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("Config file does not exist: %s", configPath)
+	}
+
+	var cfg Config
+
+	err := cleanenv.ReadConfig(configPath, &cfg);
+
+	if err != nil {
+		log.Fatalf("Cannot read the config file: %s", err.Error())
+	}
+
+	return &cfg
+}
